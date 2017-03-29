19f70fec5e862c09499326bf992f99d18645b5c1
Ander <anderson.ro.al@hotmail.com>
2017-03-28 22:29:51 -0300

    commitando

--- app/src/main/java/br/fadep/mobile/ActLogin.java
+++ app/src/main/java/br/fadep/mobile/ActLogin.java
@@ -9,5 +9,6 @@ public class ActLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_login);
+
     }
 }
